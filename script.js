65064a738b15ee9ec82163f7407c7721598ae3ac
Matheus Carvalho <matheusdocarvalho@gmail.com>
2022-09-26 22:18:39 -0300

    upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let menuMobile = document.querySelector('.menuMobile')
+let botaoMenu = document.querySelector('.botaoMenu')
+
+let aberto = false // flag - variável de controle - estado de determinada aplicação
+
+function abrirOuFecharMenu(){
+
+    if(aberto === true){
+        aberto = false
+        menuMobile.classList.remove('aberto')
+        botaoMenu.innerHTML = '☰'
+    }else if (aberto === false){
+        aberto = true
+        menuMobile.classList.add('aberto')
+        botaoMenu.innerHTML= '<b>Fechar Menu</b>'
+    }
+}
